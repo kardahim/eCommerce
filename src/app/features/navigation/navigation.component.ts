8df83fb42b7f70a0d513b55bbd37081902bb456a
Paweł Ciszewski <pawel.l.ciszewski@gmail.com>
2024-09-09 19:37:54 +0200

    add router

--- src/app/features/navigation/navigation.component.ts
+++ src/app/features/navigation/navigation.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NgIf],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
 
+  // get from local storage
+  cart_items_number: number = 3;
 }
