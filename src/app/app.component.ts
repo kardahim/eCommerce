cc0369b2db0795dcc96dfe741c9b8452c361a77e
Paweł Ciszewski <pawel.l.ciszewski@gmail.com>
2024-09-11 12:39:43 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './features/navigation/navigation.component';
 import { CartComponent } from './features/cart/cart.component';
+import { FooterComponent } from './features/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent, CartComponent],
+  imports: [RouterOutlet, NavigationComponent, CartComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
